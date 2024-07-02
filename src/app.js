2af85b01fd27676c3b92ca79a4fd3d09c3ee5ad5
maulik231 <maulikchodvadiya231@gmail.com>
2024-07-02 14:49:42 +0530

    updated cord

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const router = require("./routes");
 require('dotenv').config();
 const corsOptions = {
-  origin: "http://localhost:3133", // Allow requests from this origin
+  'Access-Control-Allow-Origin': "*",
+  origin: "*", // Allow requests from this origin
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true, // Allow cookies to be sent with requests
   optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
